c073271f9e715841e476f7346cec5e3582d3fd77
Yannis Sgarra <hello@yannissgarra.com>
2022-05-13 00:21:09 +0200

    test: prepend test config

--- src/DependencyInjection/WebmunkeezSecurityExtension.php
+++ src/DependencyInjection/WebmunkeezSecurityExtension.php
@@ -71,5 +71,22 @@ final class WebmunkeezSecurityExtension extends Extension implements PrependExte
                 'ROLE_EDITOR' => 'ROLE_USER',
             ],
         ]);
+
+        // By default, password hashers are resource intensive and take time. This is
+        // important to generate secure password hashes. In tests however, secure hashes
+        // are not important, waste resources and increase test times. The following
+        // reduces the work factor to the lowest possible values.
+        if ('test' === $container->getParameter('kernel.environment')) {
+            $container->prependExtensionConfig('security', [
+                'password_hashers' => [
+                    PasswordAuthenticatedUserInterface::class => [
+                        'algorithm' => 'auto',
+                        'cost' => 4, // Lowest possible value for bcrypt
+                        'time_cost' => 3, // Lowest possible value for argon
+                        'memory_cost' => 10, // Lowest possible value for argon
+                    ],
+                ],
+            ]);
+        }
     }
 }
