0d8f9494d6af0522357f91467e83a62e0ef8c774
Yannis Sgarra <hello@yannissgarra.com>
2022-05-30 09:49:34 +0200

    fix: useless

--- src/Exception/InvalidTokenException.php
+++ src/Exception/InvalidTokenException.php
@@ -16,10 +16,4 @@ namespace Webmunkeez\SecurityBundle\Exception;
  */
 final class InvalidTokenException extends RuntimeException
 {
-    public function __construct(string $message = '', int $code = 0, ?\Throwable $previous = null)
-    {
-        $message = false === empty($message) ? $message : 'Token is invalid.';
-
-        parent::__construct($message, $code, $previous);
-    }
 }
